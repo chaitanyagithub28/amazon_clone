290cb736504e1e42427beb9e0c9967cef256a6cd
chaitanyagithub28 <chaitanyaeducation36@gmail.com>
2022-12-01 19:47:08 +0530

    coupon

--- javascript.js
+++ javascript.js
@@ -2,3 +2,15 @@ let changeMode = () => {
     let mybody = document.body;
     mybody.classList.toggle('mydark')   
 }
+
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('main').style.opacity='0.2';
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity='1';
+    let mybody = document.body;
+    mybody.classList.toggle('mydark')     
+}
